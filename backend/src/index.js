6a5bdf678ac1ef3f712aabef052400323a3c3a67
Ayush-3012 <diveshsrivastava31@gmail.com>
2025-04-22 18:12:08 +0530

    added js files in all

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./db/connect.js";
+
+import app from "./app.js"; 
+app.get("/", (req, res) => res.send("Hello, Welcome!"));
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(` Server is listening to port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDb Connection failed: ", err));
